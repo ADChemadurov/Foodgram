0ac2671b68099b07697bedb48b542a3d5f6e983a
ADChemadurov <turbohamster@yandex.ru>
2021-12-17 14:50:58 +0300

    tweaked permissions

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,11 +3,18 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsOwnerOrAdminOrReadOnly(BasePermission):
     def has_permission(self, request, view):
-        if request.method == 'POST':
-            return request.user.is_authenticated
-        return True
+        # if request.method == 'POST':
+        #     return request.user.is_authenticated
+        # return True
+        return (
+            request.method == 'POST' and request.user.is_authenticated
+            or request.method in SAFE_METHODS)
 
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS or request.user.is_superuser:
-            return True
-        return request.user == obj.author
+        # if request.method in SAFE_METHODS or request.user.is_superuser:
+        #     return True
+        # return request.user == obj.author
+        return (
+            request.method in SAFE_METHODS
+            or request.user.is_superuser
+            or request.user == obj.author)
